29526f50f6a580e9ffd86261f4c7abb7d7411d65
ghost <akshattiwari487@gmail.com>
2024-10-11 01:24:03 +0530

    Basic logic Building of Work Flow in JS

---
+++ app.js
@@ -0,0 +1,129 @@
+const view1 = document.getElementById("view1")
+const view2 = document.getElementById("view2")
+const input__exercise = document.getElementById("input__exercise")
+const input__time =document.getElementById("input__time")
+const section1 =document.getElementById("display__exe")
+const add = document.getElementById("add");
+const end__work =document.getElementById("end__work")
+const begin__work=document.getElementById("begin__work")
+const exeArray =[]
+
+// Adding Exercise 12 -24
+add.addEventListener("click",()=>{
+   let exe = input__exercise.value;
+   let time = input__time.value;
+   exeArray.push([exe , time]);
+   console.log(exeArray);
+   addToPage(exe,time);
+})
+
+const addToPage =(exe,time)=>{
+    let element =document.createElement("div")
+   element.innerHTML= `<h4>${exe}</h4> <h5>${time}</h5>`
+   section1.appendChild(element);
+} 
+
+
+
+
+
+
+
+
+
+begin__work.addEventListener("click",()=>{
+    if(exeArray.length==0){
+        console.log("First Add Something");
+        return;
+    } 
+    startWork(exeArray.length);
+})
+
+const startWork=(count)=>{
+  let i=0;
+  runExercise(i,count)
+}
+
+const runExercise=(i,count)=>{
+  if(i>=count){
+    console.log("WorkOut Done")
+    return;
+  }
+  view1.style.display = "none";
+  view2.style.display = "block";
+
+ let breakTime =10
+ const exeElement= document.createElement("h3");
+ exeElement.innerText=`start ${exeArray[i][0]} in ${breakTime}`
+ view2.appendChild(exeElement)
+
+ let breakTimer= setInterval(()=>{
+   breakTime--
+
+if(breakTime==0){
+
+  clearInterval(breakTimer)
+  exeElement.innerText=`start ${exeArray[i][0]}`
+
+  const timeElement = document.querySelector("div")
+  timeElement.setAttribute("id","countDown")
+  timeElement.innerText="0:0"
+  view2.appendChild(timeElement)
+  beginExercise();
+}
+
+  exeElement.innerText = `start ${exeArray[i][0]} in ${breakTime}`
+  view2.appendChild(exeElement)
+ },1000)
+}
+
+
+const beginExercise=()=>{
+const startClock =()=>{
+  let time = 0
+  let count =document.getElementById("countDown")
+  const startTimer =()=>{
+      let hrs = Math.floor(time/3600) 
+      let mins =Math.floor((time%3600)/60);
+      let sec = time%60
+      count.innerHTML=`${hrs}:${mins}:${sec}`
+      time++
+  }
+ setInterval(startTimer,1000)
+}
+startClock();
+}
+
+
+
+/* const newDisplay=(i)=>{
+    view1.style.display="none";
+    let exeHeading = document.createElement("h2");
+    let sec = 
+    exeHeading.innerText= `Start ${exeArray[i][0]} in 30s`
+    view2.style.display="block"
+    view2.appendChild(exeHeading)
+ */
+    // after break ends start a Clock 
+    //Display clock in view2
+    // make a done btn
+
+
+
+ //while loop(i < exe.length)
+     // ith exe 'name' starts in break(30s)
+    //Timer
+   //Done (on clicking save data in new array and Move to next element in exearray)
+
+
+/* 
+   const breakTimer=()=>{
+         const time  = 30;
+         while(time !=0){
+         
+
+
+
+          time--
+         }
+   } */

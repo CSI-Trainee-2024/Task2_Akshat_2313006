4b9009f1b47caed933ef89dae5476b3e25e4085f
ghost <akshattiwari487@gmail.com>
2024-10-11 15:10:59 +0530

    complete JS Logic AND Result Display

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ const end__work =document.getElementById("end__work")
 const begin__workout=document.getElementById("begin__work")
 const exeArray =[]
 const timeStorage=[]
-// Adding Exercise 12 -24
+// Adding Exercise 
 add.addEventListener("click",()=>{
    let exe = input__exercise.value;
    let time = input__time.value;
-   exeArray.push([exe , time]);
+   exeArray.push({exe , time});
    console.log(exeArray);
    addToPage(exe,time);
 })
@@ -35,10 +35,11 @@ const startWork=(count)=>{
   let i=0;
   runExercise(i,count)
 }
-
+ //MOST IMPORTANT FUNCTION OF WHOLE PROGRAMME
 const runExercise=(i,count)=>{
   if(i>=count){
     console.log("WorkOut Done")  // Display Work Out Result here
+    displayResult(count,exeArray,timeStorage)
     return;
   }
   view1.style.display = "none";
@@ -46,57 +47,88 @@ const runExercise=(i,count)=>{
 
  let breakTime =10
  const exeElement= document.createElement("h3");
- exeElement.innerText=`start ${exeArray[i][0]} in ${breakTime}`
+ exeElement.innerText=`start ${exeArray[i].exe} in ${breakTime}`
  view2.innerHTML = ""; 
  view2.appendChild(exeElement)
 
+
+ // For Giving Break After Each Exercise AND at begining
  let breakTimer= setInterval(()=>{
    breakTime--
-
 if(breakTime==0){
-
-  clearInterval(breakTimer)
-  exeElement.innerText=`start ${exeArray[i][0]}`
+  clearInterval(breakTimer)  // clearing Break if Timer Reaches zeRO
+  exeElement.innerText=`start ${exeArray[i].exe}`//Derefering an Object value
 
   const timeElement = document.querySelector("div")
   timeElement.setAttribute("id","countDown")
   timeElement.innerText="0:0"
   view2.appendChild(timeElement)
-  beginExercise();
-  createDoneBtn(i);
+  const startTime = new Date().getTime();  //Capturing Time of Starting an Exercise
+  beginExercise();    
+  createDoneBtn(i,startTime); //Once Exercise Begins Create A button to move to next Exercise
 }
 
-  exeElement.innerText = `start ${exeArray[i][0]} in ${breakTime}`
+  exeElement.innerText = `start ${exeArray[i].exe} in ${breakTime}`
   view2.appendChild(exeElement)
  },1000)
 }
 
-
+//Begin Exercise Function
 const beginExercise=()=>{
 const startClock =()=>{
   let time = 0
   let count =document.getElementById("countDown")
   const startTimer =()=>{
+      time++
       let hrs = Math.floor(time/3600) 
       let mins =Math.floor((time%3600)/60);
       let sec = time%60
       count.innerHTML=`${hrs}:${mins}:${sec}`
-      time++
   }
  setInterval(startTimer,1000)
 }
-startClock()
+startClock() //Calling to start Stop Watch
 }
 
-const createDoneBtn =(i)=>{
+//Creating a Done Button
+const createDoneBtn =(i,startTime)=>{
 const doneBtn =document.createElement("button")
 doneBtn.setAttribute("id","doneBtn")
 doneBtn.innerText="Done"
 view2.appendChild(doneBtn)
+
+//Adding What Action to perform After Clicking Done button
 doneBtn.addEventListener("click",()=>{
-  //Sore timing to new array
-  runExercise(i + 1, exeArray.length); 
+  //Sore timing to new array as an object inside
+  const endTime = new Date().getTime();
+  const totalTime = Math.floor((endTime-startTime)/1000)
+  timeStorage.push({
+    exercise: exeArray[i][0],
+    timeTaken: totalTime
+  })
+  runExercise(i + 1, exeArray.length); //Loop Back to run next exercise
 })
 }
 
+//Displaying The Result by Making a Table
+const displayResult= (count,exeArray,timeStorage)=>{
+  view2.innerHTML=""
+  const table =document.createElement("table");
+  table.innerHTML=` <thead>
+        <tr>
+            <th>Exercise</th>
+            <th>Estimate</th>
+            <th>Time Taken</th>
+        </tr>
+    </thead>`
+    view2.appendChild(table)
+     for( let a=0;a<count;a++){
+      let newRow =document.createElement("tr")
+      newRow.innerHTML=`<td>${exeArray[a].exe}</td>
+                        <td>${exeArray[a].time}</td>
+                        <td>${timeStorage[a].timeTaken}</td>
+                       `
+      table.appendChild(newRow);
+     }
+}
 
